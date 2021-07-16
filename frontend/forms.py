0bf44c2c711235b2ffa69e3e775b9e30e52bd14e
ATARI Omar <atari.omar@gmail.com>
2021-07-16 11:56:03 +0200

    Add tweets exporter (#10)

--- frontend/forms.py
+++ frontend/forms.py
@@ -3,10 +3,11 @@ from wtforms import StringField, SubmitField, SelectField
 from wtforms.fields.html5 import DateField
 from wtforms.validators import DataRequired
 
-# class ExportForm(FlaskForm):
-#   term = StringField('Recherche', validators=[DataRequired()])
-#   day_start = DateField('Date de début', format='%Y-%m-%d')
-#   hour_start = StringField('Heure de début', validators=[DataRequired()])
-#   day_end = DateField('Date de fin', format='%Y-%m-%d')
-#   hour_end = StringField('Heure de fin', validators=[DataRequired()])
-#   submit = SubmitField('Exporter')
+class ExportForm(FlaskForm):
+  term = StringField('Recherche')
+  author = StringField('Auteur')
+  day_start = DateField('Date de début', format='%Y-%m-%d')
+  hour_start = StringField('Heure de début', validators=[DataRequired()])
+  day_end = DateField('Date de fin', format='%Y-%m-%d')
+  hour_end = StringField('Heure de fin', validators=[DataRequired()])
+  submit = SubmitField('Exporter')
